58d811e86a5c04884cbda2fb472a8574c1fc027f
sbx_user1060 <sbx_user1060@ip-10-128-94-79.eu-west-2.compute.internal>
2019-01-17 08:52:04 +0000

    Thu Jan 17 08:52:04 UTC 2019

--- src/BeFaster.App.Tests/Solutions/CHK/CheckoutSolutionTests.cs
+++ src/BeFaster.App.Tests/Solutions/CHK/CheckoutSolutionTests.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BeFaster.App.Solutions.SUM;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,16 @@ using System.Threading.Tasks;
 
 namespace BeFaster.App.Tests.Solutions.CHK
 {
-    class CheckoutSolutionTests
+    [TestFixture]
+    public class CheckoutSolutionTests
     {
+        private SumSolution _solution;
 
+        [SetUp]
+        public void SetUp()
+        {
+            _solution = new SumSolution();
+        }
     }
 }
+
